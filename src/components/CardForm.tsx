9ea80e24d8260247901494dee1d3ac464ca9ba79
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-28 20:52:50 +0400

    main

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -2,60 +2,72 @@ export default function CardForm() {
   return (
     <form
       className="mt-[9.2rem] flex flex-col text-start text-[#21092f]
-   text-[1.2rem] font-medium tracking-[0.2rem] uppercase px-[2.4rem] gap-[2rem]"
+   text-[1.2rem] font-medium px-[2.4rem] gap-[2rem]"
     >
       <div className="flex flex-col gap-[0.9rem]">
-        <p>Cardholder Name</p>
+        <p className="uppercase tracking-[0.2rem]">Cardholder Name</p>
         <input
           type="text"
           placeholder="e.g. Jane Appleseed"
           className="border border-[#dfdee0] 
-          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none"
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
+          text-[1.8rem]"
         />
       </div>
 
       <div className="flex flex-col gap-[0.9rem]">
-        <p>Card Number</p>
+        <p className="uppercase tracking-[0.2rem]">Card Number</p>
         <input
           type="text"
           placeholder="e.g. 1234 5678 9123 0000"
           className="border border-[#dfdee0] 
-          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none"
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
+          text-[1.8rem]"
         />
       </div>
 
       <div className="flex gap-[1.1rem]">
-        <div className="flex flex-col">
-          <p>Exp. Date (MM/YY)</p>
+        <div className="flex flex-col gap-[0.9rem]">
+          <p className="uppercase tracking-[0.2rem]">Exp. Date (MM/YY)</p>
           <div className="flex gap-[0.8rem]">
             <input
               type="text"
               placeholder="MM"
               className="border border-[#dfdee0] 
           rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
-          w-[7.2rem]"
+          w-[7.2rem]
+          text-[1.8rem]"
             />
             <input
               type="text"
               placeholder="YY"
               className="border border-[#dfdee0] 
           rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
-          w-[7.2rem]"
+          w-[7.2rem]
+          text-[1.8rem]"
             />
           </div>
         </div>
-        <div>
-          <p>CVC</p>
+        <div className="w-full flex flex-col gap-[0.9rem]">
+          <p className="uppercase tracking-[0.2rem]">CVC</p>
           <input
             type="text"
             placeholder="e.g. 123"
             className="border border-[#dfdee0] 
           rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
-          w-full"
+          w-full
+          text-[1.8rem]"
           />
         </div>
       </div>
-      <button>Confirm</button>
+      <button
+        className="w-full bg-[#21092f] 
+      text-[#fff] py-[1.5rem] rounded-[0.8rem]
+      text-[1.8rem]
+      font-medium cursor-[pointer]"
+      >
+        Confirm
+      </button>
     </form>
   );
 }
