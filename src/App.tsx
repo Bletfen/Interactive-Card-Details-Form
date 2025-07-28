e6a4ffc7e2d2ec861961ef81dc3e5910fc6d7858
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-28 20:33:26 +0400

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Cards from "./components/Cards";
+import CardForm from "./components/CardForm";
 function App() {
   return (
     <>
       <Cards />
+      <CardsForm />
     </>
   );
 }
