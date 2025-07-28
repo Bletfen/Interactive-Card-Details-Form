59efca3db0391ba727206ac242e4bac7d1fe2eda
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-28 20:28:35 +0400

    main

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,7 +4,7 @@ export default function Cards() {
       className="w-full 
     bg-[url('/images/bg-main-mobile.png')] 
     h-[24rem] bg-no-repeat bg-cover
-    flex flex-col pr-[1.6rem] pt-[3.2rem] relative"
+    flex flex-col pr-[1.6rem] pt-[3.2rem] pl-[1.6rem] relative"
     >
       <div
         className="w-[28.6rem] 
@@ -23,10 +23,35 @@ export default function Cards() {
       </div>
 
       <div
-        className="absolute w-[28.5rem] h-[15.6rem] 
-      pt-[1.7rem] px-[1.9rem]
-      bg-[url(/images/bg-card-front.png)] bg-cover bg-no-repeat bg-center"
-      ></div>
+        className="absolute w-[28.5rem] 
+      pt-[1.7rem] px-[1.9rem] pb-[2rem]
+      bg-[url(/images/bg-card-front.png)] bg-cover bg-no-repeat
+      rounded-md text-[#fff] font-medium mt-[9.5rem]"
+      >
+        <svg
+          width="54"
+          height="30"
+          viewBox="0 0 54 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <ellipse cx="15.0932" cy="15" rx="15.0932" ry="15" fill="white" />
+          <path
+            d="M47.2081 8.75C50.686 8.75004 53.5001 11.5512 53.5001 15C53.5001 18.4488 50.686 21.25 47.2081 21.25C43.7303 21.25 40.9161 18.4489 40.9161 15C40.9161 11.5511 43.7303 8.75 47.2081 8.75Z"
+            stroke="white"
+          />
+        </svg>
+        <p className="text-[1.8rem] tracking-[2.2px] mt-[3.7rem]">
+          9591 6489 6389 1011
+        </p>
+        <div
+          className="uppercase flex
+        justify-between mt-[1.7rem]"
+        >
+          <span>felicia leire</span>
+          <span>09/26</span>
+        </div>
+      </div>
     </div>
   );
 }
