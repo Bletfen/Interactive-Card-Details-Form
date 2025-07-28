d496e026db820196b9491198f2cdd8e7efb179e7
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-28 20:47:59 +0400

    main

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -2,27 +2,57 @@ export default function CardForm() {
   return (
     <form
       className="mt-[9.2rem] flex flex-col text-start text-[#21092f]
-   text-[1.2rem] font-medium tracking-[0.2rem] uppercase px-[2.4rem]"
+   text-[1.2rem] font-medium tracking-[0.2rem] uppercase px-[2.4rem] gap-[2rem]"
     >
       <div className="flex flex-col gap-[0.9rem]">
         <p>Cardholder Name</p>
-        <input type="text" placeholder="e.g. Jane Appleseed" />
+        <input
+          type="text"
+          placeholder="e.g. Jane Appleseed"
+          className="border border-[#dfdee0] 
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none"
+        />
       </div>
 
-      <div>
+      <div className="flex flex-col gap-[0.9rem]">
         <p>Card Number</p>
-        <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+        <input
+          type="text"
+          placeholder="e.g. 1234 5678 9123 0000"
+          className="border border-[#dfdee0] 
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none"
+        />
       </div>
 
-      <div className="flex">
+      <div className="flex gap-[1.1rem]">
         <div className="flex flex-col">
           <p>Exp. Date (MM/YY)</p>
-          <input type="text" placeholder="MM" />
-          <input type="text" placeholder="YY" />
+          <div className="flex gap-[0.8rem]">
+            <input
+              type="text"
+              placeholder="MM"
+              className="border border-[#dfdee0] 
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
+          w-[7.2rem]"
+            />
+            <input
+              type="text"
+              placeholder="YY"
+              className="border border-[#dfdee0] 
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
+          w-[7.2rem]"
+            />
+          </div>
         </div>
         <div>
           <p>CVC</p>
-          <input type="text" placeholder="e.g. 123" />
+          <input
+            type="text"
+            placeholder="e.g. 123"
+            className="border border-[#dfdee0] 
+          rounded-[0.8rem] pl-[1.6rem] py-[1.1rem] outline-none
+          w-full"
+          />
         </div>
       </div>
       <button>Confirm</button>
