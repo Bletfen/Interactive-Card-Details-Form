68aac5267aa2cdc01df4dc8cc11a79bc93101323
Giorgi <giorgigrigolishvili@gmail.com>
2025-07-30 23:28:56 +0400

    errorHandling

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,11 @@ import { useState } from "react";
 import type { TerrorState } from "./types";
 function App() {
   const [submit, setSubmit] = useState<boolean>(false);
-  const [errors, setErrors] = useState<TerrorState>({
-    cardHolderName: false,
-    cardNumber: false,
-    expMonth: false,
-    expYear: false,
-    cvc: false,
-  });
   return (
     <div className="flex flex-col xl:flex-row xl:gap-[34.9rem]">
       <Cards />
       {submit ? (
-        <SubmitText
-          setSubmit={setSubmit}
-          errors={errors}
-          setErrors={setErrors}
-        />
+        <SubmitText setSubmit={setSubmit} />
       ) : (
         <CardForm setSubmit={setSubmit} />
       )}
